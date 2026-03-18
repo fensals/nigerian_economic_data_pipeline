7e156011baa3da7c13275d161330e2bdbfe3d9c9
Femi Samuel <fensalsdx@gmail.com>
2026-03-18 17:35:20 +0100

    update the pipeline_dag and docker compose file to work well with airflow and also the git ignore.

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -1,3 +1,4 @@
+import os
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
@@ -18,21 +19,29 @@ dag = DAG(
     catchup=False
 )
 
+#path to location on local PC
+local_spark_apps_path = 'C:/Users/USER/Documents/Data Engineering/cbn/spark-apps'
 
-local_spark_apps_path = '/mnt/c/Users/USER/Documents/Data Engineering/cbn/spark-apps' 
 
-# Define the task to submit the Spark job using DockerOperator
+#spark job using docker operator to run the master_etl.py script inside a Docker container
+
 submit_spark_job = DockerOperator(
     task_id='submit_spark_job',
     image='quay.io/jupyter/pyspark-notebook:latest', # Use the image that has Spark
     api_version='auto',
     auto_remove=True,
-    # The command to run your ETL script
-    command="bash -c 'pip install hdfs requests && spark-submit --packages org.postgresql:postgresql:42.7.2 /opt/spark-apps/master_etl.py'",
+    mount_tmp_dir=False,  # <--- FIXES the "Bad Request" /tmp error
+    command="bash -c 'pip install hdfs requests && spark-submit --jars /opt/spark-apps/postgresql-42.7.2.jar /opt/spark-apps/master_etl.py'",
     docker_url='unix://var/run/docker.sock',
-    network_mode='cbn_hadoopnet', # MUST be on the same network as HDFS/Postgres
+
+    network_mode='cbn_hadoopnet',
+    dns=['8.8.8.8'],
+    environment={
+        'DB_USER': os.getenv('DB_USER'),
+        'DB_PASSWORD': os.getenv('DB_PASSWORD'),
+    },
+    
     mounts=[
-        # Mount the script folder so the container can see the code
         Mount(source=local_spark_apps_path, target='/opt/spark-apps', type='bind'),
     ],
     dag=dag
